d1f56ed37bab87c6b08a96720db3c2921cf9ff4a
deejaypromzy <52883917+deejaypromzy@users.noreply.github.com>
2019-10-27 05:46:45 +0000

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -78,13 +78,10 @@ public class DetailActivity extends AppCompatActivity {
         createShareIntent();
         return super.onOptionsItemSelected(item);
     }
-
-
     private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
         startActivity(shareIntent);
     }
-
 }
